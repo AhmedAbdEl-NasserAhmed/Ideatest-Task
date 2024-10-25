06c9413f1c6e26e583b0fe6f1035c9a1d0545861
unknown <nasserahmed093@gmail.com>
2024-10-26 01:16:55 +0300

    start implementing a login page and installation use form hook is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      hello
+    <div className="bg-main p-14 h-screen flex items-center justify-between gap-10 ">
+      <div className="w-1/2 border-2 border-red-500 h-full rounded-2xl">&npsb;</div>
+      <div className="border-2 border-gray-400 h-full p-10 w-1/2 rounded-2xl">
+        Login Page
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,15 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+        main: "#fafafa"
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 export default config;
